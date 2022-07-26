b2013b93697a13a2122340ad7cc0b7aaf393e45c
Camilo Acuña <camilo.aku@gmail.com>
2022-07-26 19:23:14 +0200

    feat(api): add new endpoint to return the user balance

--- src/pkg/api/transaction/router.ts
+++ src/pkg/api/transaction/router.ts
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import { Context } from "../type";
-import { getDepositsHandler, getWithdrawsHandler } from "./handler";
+import { getDepositsHandler, getWithdrawsHandler, getBalancesHandler } from "./handler";
 
 export const router = (ctx: Context) => {
   const router = Router();
 
   router.get("/transactions/:wallet/deposits/:contract_id", getDepositsHandler(ctx));
   router.get("/transactions/:wallet/withdraws/:contract_id", getWithdrawsHandler(ctx));
+  router.get("/transactions/:wallet/balances/:contract_id", getBalancesHandler(ctx));
 
   return router;
 };
