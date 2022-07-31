61293af5dad22f99d9d6308a5c6b6cb642493352
Camilo Acuña <camilo.aku@gmail.com>
2022-07-31 15:01:23 +0300

    refactor(api): move type responses to our folders

--- src/pkg/api/api.ts
+++ src/pkg/api/api.ts
@@ -2,19 +2,24 @@
 import http from "http";
 import express from "express";
 import { IDatabase } from "../database/type";
+import { ITokenManager } from "pkg/token/type";
 
 // load routers
 import * as wallet from "./wallet";
 import * as transaction from "./transaction";
+import * as token from "./token";
+import { Networks } from "../../synchronizer";
 
 export class Server {
   private port: number;
   private db: IDatabase;
   private server: http.Server;
+  private ns: Networks;
 
-  constructor(port: number, db: IDatabase) {
+  constructor(port: number, db: IDatabase, networks: Networks) {
     this.port = port;
     this.db = db;
+    this.ns = networks;
   }
 
   public run(): void {
@@ -22,6 +27,7 @@ export class Server {
 
     app.use("/api/v1", transaction.router({ db: this.db }));
     app.use("/api/v1", wallet.router({ db: this.db }));
+    app.use("/api/v1", token.router({ ns: this.ns }));
 
     this.server = app.listen(this.port, async () => {
       console.log(`server are listening on port ${this.port}`);
--- src/pkg/api/type.ts
+++ src/pkg/api/type.ts
@@ -1,50 +1,5 @@
-import { Transaction, Wallet } from "../../sychronizer";
-import { IDatabase } from "../database/type";
-
-export type Context = {
-  db: IDatabase;
-};
-
-type Response = {
+export type Response = {
   data?: unknown;
   meta?: unknown;
   error?: string;
-};
-
-export type DepositsResponse = Response & {
-  data?: {
-    deposits: Transaction[];
-  };
-  meta?: {
-    count: number;
-  };
-};
-
-export type WithdrawsResponse = Response & {
-  data?: {
-    withdraws: Transaction[];
-  };
-  meta?: {
-    count: number;
-  };
-};
-
-export type BalacesResponse = Response & {
-  data?: {
-    deposits: string;
-    withdraws: string;
-    balance: string;
-  };
-};
-
-export type ListWalletsResponse = Response & {
-  meta?: {
-    count: number;
-  };
-};
-
-export type LoginWalletResponse = Response & {
-  data?: {
-    wallet: Wallet;
-  };
 };
