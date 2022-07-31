856c24cad27c1f2494ded517badb750f819ee305
Camilo Acuña <camilo.aku@gmail.com>
2022-07-31 14:58:45 +0300

    refactor(cmd): add support to multiples chain

--- src/cmd/synchronizer/index.ts
+++ src/cmd/synchronizer/index.ts
@@ -12,10 +12,14 @@ import { Server } from "../../pkg/api";
 import { Database } from "../../pkg/database";
 import { Cron } from "../../pkg/cron";
 import { EnvParser } from "../../pkg/env-parser";
+import { Cache } from "../../pkg/cache";
+import { TokenManager } from "../../pkg/token";
+
+import { ethers } from "ethers";
+import { Networks, Network } from "synchronizer";
 
 // define logger
 import log from "pino";
-import { ethers } from "ethers";
 const logger = log();
 
 // define global instances
@@ -28,25 +32,50 @@ async function main() {
     // get env values
     const env = EnvParser();
 
+    // define networks for service
+    const networks: Networks = {} as Networks;
+
     // run database package
-    db = new Database(env.str.DATABASE_URL);
+    db = new Database(env.DATABASE_URL);
     await db.connect();
 
     // run api package
-    api = new Server(env.num.PORT, db);
+    api = new Server(env.PORT, db, networks);
     api.run();
 
-    // initialize contract
-    const provider = new ethers.providers.AlchemyProvider(env.str.NETWORK, env.str.ALCHEMY_API_KEY);
-    const contract = new ethers.Contract(
-      env.str.CONTRACT_ADDRESS,
-      MockSupplyAave__factory.abi,
-      provider
-    ) as MockSupplyAave;
+    // initialize provider and contract
+    for (const networkName in env.NETWORK) {
+      const network = env.NETWORK[networkName as Network];
+      if (!network) {
+        throw new Error(`invalid network_name=${networkName}`);
+      }
 
-    // run cron package
-    cron = new Cron(env.num.INTERVAL_SECONDS, db, contract);
-    cron.run();
+      // initialize ethers js
+      const provider = new ethers.providers.AlchemyProvider(networkName, network.ALCHEMY_API_KEY);
+
+      // intialize contract
+      const contract = new ethers.Contract(
+        network.CONTRACT_ADDRESS,
+        MockSupplyAave__factory.abi,
+        provider
+      ) as MockSupplyAave;
+
+      // intialize cache package
+      const cache = new Cache();
+
+      // initialize token manager package
+      const tokenManager = new TokenManager(provider.connection.url, provider, cache);
+
+      // initialize cron package
+      cron = new Cron(networkName as Network, env.INTERVAL_SECONDS, db, contract);
+      cron.run();
+
+      // add cron and token manager to networks definition
+      networks[networkName as Network] = {
+        cron,
+        tm: tokenManager,
+      };
+    }
   } catch (err) {
     logger.error(err);
     process.exit(1);
